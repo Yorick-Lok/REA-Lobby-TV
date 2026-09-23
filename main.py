2be3276a15cecc45c148a103c8beba7286fc59a9
mhoek2 <153363103+mhoek2@users.noreply.github.com>
2026-09-23 11:17:55 +0200

    Use http port 5000 and allow LAN connections

--- main.py
+++ main.py
@@ -53,8 +53,9 @@ class Server:
 
     def run( self ) -> None: 
             self.app.run(
-                port=80,
-                host="127.0.0.1"
+                debug=False,
+                port=5000,
+                host="0.0.0.0"
             )
 
 if __name__ == "__main__":
